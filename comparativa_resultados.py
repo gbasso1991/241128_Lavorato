6f4e9ed15d9636fb65b719d70cde3a2d794bb4f2
[Giuliano Basso] <[giulianoandresbasso@gmail.com]>
2024-12-02 23:23:53 -0300

    Agregados insets y calculo de Hc

--- comparativa_resultados.py
+++ comparativa_resultados.py
@@ -11,6 +11,7 @@ from scipy.interpolate import interp1d
 from uncertainties import ufloat, unumpy
 from datetime import datetime,timedelta
 import matplotlib as mpl
+from uncertainties import ufloat, unumpy
 from scipy.interpolate import CubicSpline,PchipInterpolator
 #%%
 #%% LECTOR RESULTADOS
@@ -102,6 +103,34 @@ def lector_ciclos(filepath):
     M_Am  = pd.Series(data['Magnetizacion_(A/m)']).to_numpy(dtype=float)#A/m
     
     return t,H_Vs,M_Vs,H_kAm,M_Am,metadata
+
+def calcular_hc(H, m):
+    # Encuentra los índices donde m cruza el eje x (cambio de signo)
+    # el error es la diferencia entre valor negativo y positivo
+    cruces = np.where(np.diff(np.sign(m)) != 0)[0]
+
+    hc_valores = []
+    for i in cruces:
+        # Interpolación lineal para encontrar el cruce exacto
+        h1, h2 = H[i], H[i + 1]
+        m1, m2 = m[i], m[i + 1]
+        h_c = h1 - m1 * (h2 - h1) / (m2 - m1)
+        hc_valores.append(h_c)
+    
+    # Obtén valores positivos y negativos
+    hc_positivos = [h for h in hc_valores if h > 0]
+    hc_negativos = [h for h in hc_valores if h < 0]
+
+    # Calcula el promedio absoluto de los positivos y negativos
+    if hc_positivos and hc_negativos:
+        hc_promedio = (np.mean(hc_positivos) + abs(np.mean(hc_negativos))) / 2
+        hc_err=    hc_valores[0]+hc_valores[1] 
+    else:
+        hc_promedio = None  # Si no hay suficientes cruces
+    print('Hc = ', hc_promedio,hc_valores)
+    return hc_promedio, hc_err
+
+
 #%%
 
 ciclos_100 = glob(os.path.join('resultados_100', '*ciclo_promedio*'),recursive=True)
@@ -116,67 +145,99 @@ _,_,_,H100_1,M_100_1,meta_100_1=lector_ciclos(ciclos_100[0])
 _,_,_,H100_2,M_100_2,meta_100_2=lector_ciclos(ciclos_100[1]) 
 _,_,_,H100_3,M_100_3,meta_100_3=lector_ciclos(ciclos_100[2]) 
 _,_,_,H100_4,M_100_4,meta_100_4=lector_ciclos(ciclos_100[3])
-concentracion_100 = np.array([meta_100_1['Concentracion_g/m^3'],
-                             meta_100_2['Concentracion_g/m^3'],
-                             meta_100_3['Concentracion_g/m^3']
-                             ,meta_100_4['Concentracion_g/m^3']])/1000 #g/L == kg/m³ 
-
 
 _,_,_,H97_1,M_97_1,meta_97_1=lector_ciclos(ciclos_97[0]) 
 _,_,_,H97_2,M_97_2,meta_97_2=lector_ciclos(ciclos_97[1]) 
 _,_,_,H97_3,M_97_3,meta_97_3=lector_ciclos(ciclos_97[2]) 
 _,_,_,H97_4,M_97_4,meta_97_4=lector_ciclos(ciclos_97[3])
  
+#%% divido por concentracion
+concentracion_100 = np.array([meta_100_1['Concentracion_g/m^3'],
+                             meta_100_2['Concentracion_g/m^3'],
+                             meta_100_3['Concentracion_g/m^3']
+                             ,meta_100_4['Concentracion_g/m^3']])/1000 #g/L == kg/m³ 
+
 concentracion_97 = np.array([meta_97_1['Concentracion_g/m^3'],
                              meta_97_2['Concentracion_g/m^3'],
                              meta_97_3['Concentracion_g/m^3']
                              ,meta_97_4['Concentracion_g/m^3']])/1000 #g/L == kg/m³ 
- 
+
+m_100_1 = M_100_1/concentracion_100[0]
+m_100_2 = M_100_2/concentracion_100[1]
+m_100_3 = M_100_3/concentracion_100[2]
+m_100_4 = M_100_4/concentracion_100[3]
+print('Coercitivo 100')
+Hc_100_1_mean,Hc_100_1_err=calcular_hc(H100_1,m_100_1)
+Hc_100_2_mean,Hc_100_2_err=calcular_hc(H100_2,m_100_2)
+Hc_100_3_mean,Hc_100_3_err=calcular_hc(H100_3,m_100_3)
+Hc_100_4_mean,Hc_100_4_err=calcular_hc(H100_4,m_100_4)
+
+Hc_100_1=ufloat(Hc_100_1_mean,Hc_100_1_err)
+Hc_100_2=ufloat(Hc_100_2_mean,Hc_100_2_err)
+Hc_100_3=ufloat(Hc_100_3_mean,Hc_100_3_err)
+Hc_100_4=ufloat(Hc_100_4_mean,Hc_100_4_err)
+
+m_97_1 = M_97_1/concentracion_97[0]
+m_97_2 = M_97_2/concentracion_97[1]
+m_97_3 = M_97_3/concentracion_97[2]
+m_97_4 = M_97_4/concentracion_97[3]
+print('Coercitivo 97')
+Hc_97_1_mean,Hc_97_1_err=calcular_hc(H97_1,m_97_1)
+Hc_97_2_mean,Hc_97_2_err=calcular_hc(H97_2,m_97_2)
+Hc_97_3_mean,Hc_97_3_err=calcular_hc(H97_3,m_97_3)
+Hc_97_4_mean,Hc_97_4_err=calcular_hc(H97_4,m_97_4)
+
+Hc_97_1=ufloat(Hc_97_1_mean,Hc_97_1_err)
+Hc_97_2=ufloat(Hc_97_2_mean,Hc_97_2_err)
+Hc_97_3=ufloat(Hc_97_3_mean,Hc_97_3_err)
+Hc_97_4=ufloat(Hc_97_4_mean,Hc_97_4_err)
 #%% ploteo ciclos 
 
 fig, (ax1,ax2)=plt.subplots(ncols=2,figsize=(12,5),constrained_layout=True,sharey=True,sharex=True)
 
-ax1.plot(H100_1,M_100_1/concentracion_100[0], label=labels_100[0]+ f' - {concentracion_100[0]:.2f} g/L')
-ax1.plot(H100_3,M_100_3/concentracion_100[2], label=labels_100[2]+ f' - {concentracion_100[2]:.2f} g/L')
-ax1.plot(H100_4,M_100_4/concentracion_100[3], label=labels_100[3]+ f' - {concentracion_100[3]:.2f} g/L')
-ax1.plot(H100_2,M_100_2/concentracion_100[1], label=labels_100[1]+ f' - {concentracion_100[1]:.2f} g/L')
-axin1 = ax1.inset_axes([0.56, 0.06, 0.42, 0.4],)
-axin1.plot(H100_1,M_100_1/concentracion_100[0])
-axin1.plot(H100_2,M_100_2/concentracion_100[1])
-axin1.plot(H100_3,M_100_3/concentracion_100[2])
-axin1.plot(H100_4,M_100_4/concentracion_100[3])
-axin1.set_xlim(-60e2,60e2)           
-axin1.set_ylim(-10,10)           
+ax1.plot(H100_1,m_100_1, label=labels_100[0]+ f' - {concentracion_100[0]:.2f} g/L')
+ax1.plot(H100_3,m_100_3, label=labels_100[2]+ f' - {concentracion_100[2]:.2f} g/L')
+ax1.plot(H100_4,m_100_4, label=labels_100[3]+ f' - {concentracion_100[3]:.2f} g/L')
+ax1.plot(H100_2,m_100_2, label=labels_100[1]+ f' - {concentracion_100[1]:.2f} g/L')
+ax1.set_title('100',fontsize=14)
 
-           
-# ax1.plot(H100_3,M_100_3/concentracion_100[2], label=labels_100[2]+ f' - {concentracion_100[2]:.2f} g/L')
-# ax1.plot(H100_4,M_100_4/concentracion_100[3], label=labels_100[3]+ f' - {concentracion_100[3]:.2f} g/L')
-# ax1.plot(H100_2,M_100_2/concentracion_100[1], label=labels_100[1]+ f' - {concentracion_100[1]:.2f} g/L')
+axin1 = ax1.inset_axes([0.00, -1.0, 0.95,0.85],)
+axin1.plot(H100_1,m_100_1,'.-', label=labels_100[0]+ f' - {Hc_100_1:.0f} A/m')
+axin1.plot(H100_3,m_100_3,'.-', label=labels_100[2]+ f' - {Hc_100_3:.0f} A/m')
+axin1.plot(H100_4,m_100_4,'.-', label=labels_100[3]+ f' - {Hc_100_4:.0f} A/m')
+axin1.plot(H100_2,m_100_2,'.-', label=labels_100[1]+ f' - {Hc_100_2:.0f} A/m')
+axin1.set_xlim(-80e2,80e2)           
+axin1.set_ylim(-20,20)           
 
-ax2.plot(H97_1,M_97_1/concentracion_97[0], label=labels_97[0]+ f' - {concentracion_97[0]:.2f} g/L')
-ax2.plot(H97_3,M_97_3/concentracion_97[2], label=labels_97[2]+ f' - {concentracion_97[2]:.2f} g/L')
-ax2.plot(H97_4,M_97_4/concentracion_97[3], label=labels_97[3]+ f' - {concentracion_97[3]:.2f} g/L')
-ax2.plot(H97_2,M_97_2/concentracion_97[1], label=labels_97[1]+ f' - {concentracion_97[1]:.2f} g/L')
+ax2.set_title('97',fontsize=14)
+ax2.plot(H97_1,m_97_1, label=labels_97[0]+ f' - {concentracion_97[0]:.2f} g/L')
+ax2.plot(H97_3,m_97_3, label=labels_97[2]+ f' - {concentracion_97[2]:.2f} g/L')
+ax2.plot(H97_4,m_97_4, label=labels_97[3]+ f' - {concentracion_97[3]:.2f} g/L')
+ax2.plot(H97_2,m_97_2, label=labels_97[1]+ f' - {concentracion_97[1]:.2f} g/L')
 
-axin2 = ax2.inset_axes([0.56, 0.06, 0.42, 0.4])
-axin2.plot(H97_1,M_97_1/concentracion_97[0])
-axin2.plot(H97_2,M_97_2/concentracion_97[1])
-axin2.plot(H97_3,M_97_3/concentracion_97[2])
-axin2.plot(H97_4,M_97_4/concentracion_97[3])
+axin2 = ax2.inset_axes([0.00, -1.0, 0.95,0.85])
+axin2.plot(H97_1,m_97_1,'.-',label=labels_97[0]+ f' - {Hc_97_1:.0f} A/m')
+axin2.plot(H97_3,m_97_3,'.-',label=labels_97[2]+ f' - {Hc_97_3:.0f} A/m')
+axin2.plot(H97_4,m_97_4,'.-',label=labels_97[3]+ f' - {Hc_97_4:.0f} A/m')
+axin2.plot(H97_2,m_97_2,'.-',label=labels_97[1]+ f' - {Hc_97_2:.0f} A/m')
 axin2.set_xlim(-80e2,80e2)           
 axin2.set_ylim(-20,20)
-
-for ai in [axin1,axin2]:
-    ai.grid()
+axin1.axhline(0, color='black',linewidth=0.8,zorder=-2)  # Horizontal line (y=0)
+axin1.axvline(0, color='black',linewidth=0.8,zorder=-2)  # Vertical line (x=0)    
+axin2.axhline(0, color='black',linewidth=0.8,zorder=-2)  # Horizontal line (y=0)
+axin2.axvline(0, color='black',linewidth=0.8,zorder=-2)  # Vertical line (x=0)    
 
 ax1.indicate_inset_zoom(axin1, edgecolor="black")
 ax2.indicate_inset_zoom(axin2, edgecolor="black")
-    
+for ai in [axin1,axin2]:
+    ai.grid()
+    ai.legend(title='Coercitivo',ncol=2,loc='upper center', bbox_to_anchor=(0.5, -0.1))
+
 for a in [ax1,ax2]:
     a.legend(ncol=1)
     a.grid()
     a.set_xlabel('H (kA/m)')
     a.set_ylabel('M/[NPM] (Am/kg)')
     
-plt.savefig('comparativa_ciclos_100_97.png',dpi=300)
+plt.savefig('comparativa_ciclos_100_97.png',dpi=400,facecolor='w')
 # %%
